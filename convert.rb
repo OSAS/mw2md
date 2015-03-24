e6417aa50fb4ad2dd68e86bfc99147f59f4161bc
Garrett LeSage <garrett@lesage.us>
2015-03-24 14:18:18 +0100

    convert __NOTOC__

--- convert.rb
+++ convert.rb
@@ -135,6 +135,7 @@ revision.sort_by { |r| r[:timestamp] }.each do |rev_info|
       },
       'atx-headers')
            .gsub(/__TOC__/, "* ToC\n{:toc}\n\n")
+           .gsub(/__NOTOC__/, '{:.no_toc}')
            .gsub(/^#/, '##')
   rescue
     puts 'Error in conversion. Skipping to next page.'
